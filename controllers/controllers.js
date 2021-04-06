f5853318ebad6c2965bd9548e0a5e8043f4bef02
Last Phantom <paritosh191200@gmail.com>
2021-04-06 18:31:55 +0530

    made url paths

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,7 +1,35 @@
-const saySomething = (req, res, next) => {
+const loginmsg = (req, res, next) => {
+    console.log("login controller");
+    console.log(req.body);
     res.status(200).json({
-        body: 'Hello from the server!'
+        body : 'Hello from to login!',
+        name : req.body.name,
+        password : req.body.password,
+        success : true,
     });
 };
 
-module.exports.saySomething = saySomething;
+const getbalancemsg = (req,res,next) => {
+    console.log("get Balance controller");
+    console.log(req.body);
+    res.status(200).json({
+        body : 'Hello from getBalance!',
+        name : req.body.name,
+        password : req.body.password,
+        balance : 10000,
+    });
+}
+
+const addbalancemsg = (req,res,next) => {
+    console.log("Add balance controller");
+    console.log(req.body);
+    res.status(200).json({
+        body : 'Hello from addBalance!',
+        amount : req.body.amount,
+        name : req.body.name,
+    });
+}
+
+module.exports.loginmsg = loginmsg;
+module.exports.getbalancemsg = getbalancemsg;
+module.exports.addbalancemsg = addbalancemsg;
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('./../controllers/controllers');
 
-router.get('/say-something', controllers.saySomething);
+router.post('/login', controllers.loginmsg);
+router.post('/addBalance', controllers.addbalancemsg);
+router.post('/getBalance', controllers.getbalancemsg);
+
+// router.post('/addBalance', function(req,res){
+//     console.log(req.body);
+//     res.status(200).json({
+//         body : 'Hello from addBalance!',
+//         amount : req.body.amount,
+//         name : req.body.name,
+//     });
+// });
 
 module.exports = router;
