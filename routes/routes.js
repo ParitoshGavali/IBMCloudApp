2aa5be45d0867891a4cf9e49d3f70d1cc2bd4658
Last Phantom <paritosh191200@gmail.com>
2021-04-12 02:58:36 +0530

    all post

--- routes/routes.js
+++ routes/routes.js
@@ -3,16 +3,7 @@ const router = express.Router();
 const controllers = require('./../controllers/controllers');
 
 router.post('/login', controllers.loginmsg);
-router.put('/addFunds', controllers.addbalancemsg);
-router.get('/getBalance', controllers.getbalancemsg);
-
-// router.post('/addBalance', function(req,res){
-//     console.log(req.body);
-//     res.status(200).json({
-//         body : 'Hello from addBalance!',
-//         amount : req.body.amount,
-//         name : req.body.name,
-//     });
-// });
+router.post('/addFunds', controllers.addbalancemsg);
+router.post('/getBalance', controllers.getbalancemsg);
 
 module.exports = router;
