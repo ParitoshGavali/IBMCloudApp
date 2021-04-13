ef0969a8872d2d0626bbe171a85932ed01e568da
Last Phantom <paritosh191200@gmail.com>
2021-04-13 22:49:24 +0530

    should be fine

--- routes/routes.js
+++ routes/routes.js
@@ -5,5 +5,8 @@ const controllers = require('./../controllers/controllers');
 router.post('/login', controllers.loginmsg);
 router.post('/addFunds', controllers.addbalancemsg);
 router.post('/getBalance', controllers.getbalancemsg);
+router.put('/addFunds', controllers.addbalancemsg);
+router.get('/getBalance', controllers.getbalancemsg);
+
 
 module.exports = router;
